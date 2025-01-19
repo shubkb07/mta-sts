f87d982bd75efb68ad54e642114845c603b2625a
Shubham Kumar Bansal <152780466+shubkb07@users.noreply.github.com>
2025-01-20 02:27:36 +0530

    Create index.mjs

---
+++ index.mjs
@@ -0,0 +1,29 @@
+// index.mjs
+import { createServer } from 'node:http';
+const server = createServer((req, res) => {
+  if (req.url === '/well-known/mta-sts.txt') {
+    const version = 'STSv1';
+    const mode = 'enforce';
+    const mx = [
+      'aspmx.l.google.com',
+      'alt1.aspmx.l.google.com',
+      'alt2.aspmx.l.google.com',
+      'alt3.aspmx.l.google.com',
+      'alt4.aspmx.l.google.com'
+    ];
+    const max_age = 86400;
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end(`version: ${version}
+mode: ${mode}
+${mx.map(entry => `mx: ${entry}`).join('\n')}
+max_age: ${max_age}`);
+  } else {
+    res.writeHead(404);
+    res.end();
+  }
+  
+});
+// starts a simple http server locally on port 3000
+server.listen(process.env.PORT || 3000, '127.0.0.1', () => {
+  console.log('Listening on 127.0.0.1:3000');
+});
